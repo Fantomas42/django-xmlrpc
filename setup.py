6e596aa8edae1b68b7a455fe604e2253af17f67a
Fantomas42 <fantomas42@gmail.com>
2012-04-06 16:11:38 +0200

    passing to version 1.4

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import os
 from setuptools import setup, find_packages
 
 setup(name = 'django-xmlrpc',
-      version = '0.1.3',
-      
+      version = '0.1.4',
+
       description = 'XML-RPC Server App for the Django framework.',
       long_description=open(os.path.join('README.rst')).read(),
       keywords = 'django, service, xmlrpc',
-      
+
       author = 'Graham Binns',
       author_email = 'graham.binns@gmail.com',
       maintainer = 'Fantomas42',
       maintainer_email = 'fantomas42@gmail.com',
       url = 'http://github.com/Fantomas42/django-xmlrpc',
-      
+
       packages = find_packages(),
       classifiers=[
           'Framework :: Django',
@@ -23,7 +23,7 @@ setup(name = 'django-xmlrpc',
           'Intended Audience :: Developers',
           'Operating System :: OS Independent',
           'Topic :: Software Development :: Libraries :: Python Modules',],
-      
+
       license = 'New BSD License',
       include_package_data = True,
       zip_safe=False
